51d2118851284cd0b618c16faf6ad485d9729271
Etienne <vile2094@usherbrooke.ca>
2023-10-30 22:23:51 -0400

    Added doxygen documentation to ROSQueue and added bad prediction logging
    - Added Doxygen documentation to ROSQueue except for its constructors since they are still in progress.
    - Changed the name of the template parameter name to follow a more standard naming.
    - Added a ROS_ERROR log if a prediction service is unavailable since all prediction would be bad and, if used for decision making, it could lead to bad decisions.

--- ros_queue/include/ros_queue/ros_queue.hpp
+++ ros_queue/include/ros_queue/ros_queue.hpp
@@ -14,17 +14,27 @@ using std::string;
 using std::invalid_argument;
 
 
-template <typename DynamicQueueType, typename TServiceClass>
-class ROSQueue: public DynamicQueueType
+/**
+ * @brief Queue wrapped with some ROS interface to interact with the queue with services and/or pointer functions.
+ * @tparam TDynamicQueueType Type of the queue used. Queue that could be used: DynamicQueue<TelementType, TServiceClass>, DynamicConvertedQueue<TelementType, TServiceClass>, InConVirtualQueue<TServiceClass> or EqConVirtualQueue<TServiceClass> 
+ * @tparam TServiceClass Type of the service used for the evaluation and prediction step. The response must have an int32 named "prediction".
+ */
+template <typename TDynamicQueueType, typename TServiceClass>
+class ROSQueue: public TDynamicQueueType
 {
     public:
+        /**
+         * @brief Member that contains meta data for queues.
+        */
         ROSQueueInfo info_;
         
+        /**
+         * @brief ROS Node handle used for the service call and make sure that a node handle exist for the life time of the ROSQueue.
+        */
         ros::NodeHandle nh_;
 
-        // Constructor
         ROSQueue(int max_queue_size, ROSQueueInfo& info, int (*arrival_prediction_fptr)(const TServiceClass&),
-         int (*transmission_prediction_fptr)(const TServiceClass&)):DynamicQueueType(max_queue_size), info_(info)
+         int (*transmission_prediction_fptr)(const TServiceClass&)):TDynamicQueueType(max_queue_size), info_(info)
         {
             if (arrival_prediction_fptr == nullptr)
             {
@@ -40,7 +50,7 @@ class ROSQueue: public DynamicQueueType
         }
 
         ROSQueue(int max_queue_size, ROSQueueInfo& info, ros::NodeHandle& nh, int (*arrival_prediction_fptr)(const TServiceClass&),
-         string transmission_prediction_service_name):DynamicQueueType(max_queue_size), info_(info), nh_(nh)
+         string transmission_prediction_service_name):TDynamicQueueType(max_queue_size), info_(info), nh_(nh)
         {
             if (arrival_prediction_fptr == nullptr)
             {
@@ -52,7 +62,7 @@ class ROSQueue: public DynamicQueueType
         }
 
         ROSQueue(int max_queue_size, ROSQueueInfo& info, ros::NodeHandle& nh, string arrival_prediction_service_name,
-        int (*transmission_prediction_fptr)(const TServiceClass&)):DynamicQueueType(max_queue_size), info_(info), nh_(nh)
+        int (*transmission_prediction_fptr)(const TServiceClass&)):TDynamicQueueType(max_queue_size), info_(info), nh_(nh)
         {
             if (transmission_prediction_fptr == nullptr)
             {
@@ -63,52 +73,73 @@ class ROSQueue: public DynamicQueueType
         }
 
         ROSQueue(int max_queue_size, ROSQueueInfo& info, ros::NodeHandle& nh, string arrival_prediction_service_name,
-        string transmission_prediction_service_name):DynamicQueueType(max_queue_size), info_(info), nh_(nh)
+        string transmission_prediction_service_name):TDynamicQueueType(max_queue_size), info_(info), nh_(nh)
         {
             arrival_service_client_ = nh.serviceClient<TServiceClass>(arrival_prediction_service_name);
             transmission_service_client_ = nh.serviceClient<TServiceClass>(transmission_prediction_service_name);
         }
 
     protected:
-
         /**
-         * @brief Method used in the evaluation process to predict what will be the arrival size. Override this method to define a specific arrival prediction behavior.
+         * @brief Method used in the evaluation process to predict what will be the arrival size. It uses a user-defined callback given by the user in the constructor if defined, otherwise it will make a service call.
+         * @param service Is a service class definition that is used for the service call and for the user-defined prediction function as data structure input and output.
          * @return Returns the converted size of the estimated arrival queue.
          */
-        virtual int arrival_prediction(const TServiceClass& states) override 
+        virtual int arrival_prediction(const TServiceClass& service) override 
         {
             if (arrival_prediction_fptr_)
             {
-                return arrival_prediction_fptr_(states);
+                return arrival_prediction_fptr_(service);
             }
             else
             {
-                //Make a local copy to respect the const arg but still allowing service call
-                TServiceClass local_service= states; 
+                //Make a local copy to respect the const arg but still allowing the service call
+                TServiceClass local_service= service; 
 
                 // Service ROS call
-                if (arrival_service_client_.call(local_service))
+                if (arrival_service_client_.exists())
                 {
-                    return local_service.response.prediction;
+                    if (arrival_service_client_.call(local_service))
+                    {
+                        return local_service.response.prediction;
+                    }
+                }
+                else
+                {
+                    ROS_DEBUG_STREAM_THROTTLE(2, "Arrival prediction service " <<  arrival_service_client_.getService() <<" is not available. Prediction will be bad and return 0.");
                 }
             }
+
+            return 0;
         }
 
-        virtual int transmission_prediction(const TServiceClass& states) override
+        /**
+         * @brief Method used in the evaluation process to predict what will be the number of departing element. It uses a user-defined callback given by the user in the constructor if defined, otherwise it will make a service call.
+         * @param service Is a service class definition that is used for the service call and for the user-defined prediction function as data structure input and output.
+         * @return Returns the converted size of the estimated transmission queue.
+         */
+        virtual int transmission_prediction(const TServiceClass& service) override
         {
             if (transmission_prediction_fptr_)
             {
-                return transmission_prediction_fptr_(states);
+                return transmission_prediction_fptr_(service);
             }
             else
             {
-                //Make a local copy to respect the const arg but still allowing service call
-                TServiceClass local_service = states; 
+                //Make a local copy to respect the const arg but still allowing the service call
+                TServiceClass local_service = service; 
 
                 // Service ROS call
-                if (transmission_service_client_.call(local_service))
+                if (transmission_service_client_.exists())
                 {
-                    return local_service.response.prediction;
+                    if (transmission_service_client_.call(local_service))
+                    {
+                        return local_service.response.prediction;
+                    }
+                }
+                else
+                {
+                    ROS_DEBUG_STREAM_THROTTLE(2, "Transmission prediction service " <<  transmission_service_client_.getService() <<" is not available. Prediction will be bad and return 0.");
                 }
             }
 
@@ -116,10 +147,22 @@ class ROSQueue: public DynamicQueueType
         }
 
     private:
+        /**
+         * @brief Service client used for the arrival prediction service calls.
+         */
         ros::ServiceClient arrival_service_client_;
+        /**
+         * @brief Function pointer for the arrival prediction service calls.
+         */
         int (*arrival_prediction_fptr_)(const TServiceClass&) = nullptr;
 
+        /**
+         * @brief Service client used for the transmission prediction service calls.
+         */
         ros::ServiceClient transmission_service_client_;
+        /**
+         * @brief Function pointer for the transmission prediction service calls.
+         */
         int (*transmission_prediction_fptr_)(const TServiceClass&) = nullptr;
 
 };
