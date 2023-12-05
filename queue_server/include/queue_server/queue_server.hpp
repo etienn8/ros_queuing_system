67556aa68ff2e5a42cb1dc4c980b8cef453d6d0d
Etienne <vile2094@usherbrooke.ca>
2023-12-04 22:42:55 -0500

    Added maps of typical queues in the queue_server
    - Added a map of three type of queues, a real queue map and two virtual ones based on if it can have a negative value or not. All those type of queues doesn't require to know the type of messages or services at compile time. Therefore, they can all be configure through config files without compilation.

--- queue_server/include/queue_server/queue_server.hpp
+++ queue_server/include/queue_server/queue_server.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <utility>
 #include <map>
+#include <memory>
 #include <string>
+#include <utility>
 
-#include "ros_queue/ros_queue.hpp"
-#include "ros_queue/ros_converted_queue.hpp"
+#include "ros_queue/ros_byte_converted_queue.hpp"
 #include "ros_queue/ros_virtual_queue.hpp"
 #include "ros_queue/lib_queue/dynamic_virtual_queue.hpp"
 
@@ -16,24 +16,18 @@ using std::string;
 
 class QueueServer
 {
-    
     public:
-        QueueServer();
-        //std::vector<ROSVirtualQueue<InConVirtualQueue, queue_server::QueueStatesPrediction>> virtual_queues_;
-
-        /*QueueServer(std::vector<ROSVirtualQueue<InConVirtualQueue, queue_server::QueueStatesPrediction>>&& virtual_queues):virtual_queues_(std::move(virtual_queues))
-        {
-
-        }*/
-
+        QueueServer(ros::NodeHandle& nh);
         
-        void addInequalityConstraintVirtualQueue(ROSInConVirtualQueue&& new_queue);
-        void addEqualityConstraintVirtualQueue(ROSInConVirtualQueue&& new_queue);
+        void addInequalityConstraintVirtualQueue(std::unique_ptr<ROSInConVirtualQueue>&& new_queue);
+        void addEqualityConstraintVirtualQueue(std::unique_ptr<ROSEqConVirtualQueue>&& new_queue);
+        void addRealQueue(std::unique_ptr<ROSByteConvertedQueue>&& new_queue);
 
-        //std::vector<????> queue_containers
-        //ROSVirtualQueue<InConVirtualQueue<>
     private:
-        std::map<string, ROSInConVirtualQueue> inequality_constraint_virtual_queues_;
-        std::map<string, ROSEqConVirtualQueue> equality_constraint_virtual_queues_;
+        std::map<string, std::unique_ptr<ROSInConVirtualQueue>> inequality_constraint_virtual_queues_;
+        std::map<string, std::unique_ptr<ROSEqConVirtualQueue>> equality_constraint_virtual_queues_;
+        std::map<string, std::unique_ptr<ROSByteConvertedQueue>> real_queues_;
+
+        ros::NodeHandle nh_;
 };
 
--- queue_server/src/queue_server.cpp
+++ queue_server/src/queue_server.cpp
@@ -1,2 +1,42 @@
 #include "queue_server/queue_server.hpp"
 
+#include <string>
+#include <utility>
+#include <map>
+
+#include "ros_queue/ros_virtual_queue.hpp"
+#include "ros_queue/lib_queue/dynamic_virtual_queue.hpp"
+
+using std::string;
+
+QueueServer::QueueServer(ros::NodeHandle& nh): nh_(nh)
+{
+
+}
+
+void QueueServer::addInequalityConstraintVirtualQueue(std::unique_ptr<ROSInConVirtualQueue>&& new_queue)
+{
+    // Extract the queue name
+    std::string queue_name = std::move(new_queue->info_.queue_name);
+    
+    // Emplace the new queue into the map
+    inequality_constraint_virtual_queues_.emplace(queue_name, std::move(new_queue));
+}
+
+void QueueServer::addEqualityConstraintVirtualQueue(std::unique_ptr<ROSEqConVirtualQueue>&& new_queue)
+{
+    // Extract the queue name
+    std::string queue_name = std::move(new_queue->info_.queue_name);
+    
+    // Emplace the new queue into the map
+    equality_constraint_virtual_queues_.emplace(queue_name, std::move(new_queue));
+}
+
+void QueueServer::addRealQueue(std::unique_ptr<ROSByteConvertedQueue>&& new_queue)
+{
+    // Extract the queue name
+    std::string queue_name = std::move(new_queue->info_.queue_name);
+    
+    // Emplace the new queue into the map
+    real_queues_.emplace(queue_name, std::move(new_queue));
+}
