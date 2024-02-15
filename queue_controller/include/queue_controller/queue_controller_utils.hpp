617f0856b4e1c2f2fe3b24cf588ed019de47d02e
Etienne <vile2094@usherbrooke.ca>
2024-02-15 14:06:43 -0500

    Added missing time measurement utils

--- queue_controller/include/queue_controller/queue_controller_utils.hpp
+++ queue_controller/include/queue_controller/queue_controller_utils.hpp
@@ -2,6 +2,7 @@
 
 #include "ros/ros.h"
 
+#include <chrono>
 #include <string>
 
 using std::string;
@@ -20,5 +21,18 @@ namespace queue_controller_utils
             client.waitForExistence();
             ROS_WARN_STREAM("Restored connection to "<<service_name);
         }
+    }
+
+    /**
+     * @brief Get the time since the time 0 in milliseconds and updates the time point.
+     * @param time_0 The time 0 from which the time difference is calculated.
+     * @param time_diff The time difference in milliseconds.
+     * @return The current time point. It could be used to chain the function with the next time point
+    */
+    std::chrono::time_point<std::chrono::system_clock> updateTimePointAndGetTimeDifferenceMS(std::chrono::time_point<std::chrono::system_clock> time_0, double& time_diff)
+   {
+        std::chrono::time_point<std::chrono::system_clock> new_time_point = std::chrono::high_resolution_clock::now();
+        time_diff = std::chrono::duration_cast<std::chrono::milliseconds>(new_time_point - time_0).count();
+        return new_time_point;
     }
 }
