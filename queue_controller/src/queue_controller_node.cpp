e460b4daf528b3883dbbafd84cb857d234b5b23a
Etienne <vile2094@usherbrooke.ca>
2024-02-16 13:29:41 -0500

    Missing template parameter

--- queue_controller/src/queue_controller_node.cpp
+++ queue_controller/src/queue_controller_node.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "queue_controller");
     ros::NodeHandle nh("~");
     
-    QueueController<METRIC_CONTROL_PREDICTION_SRV, POTENTIAL_ACTION_SET_MSG, POTENTIAL_ACTION_SET_SRV> queue_controller(nh);
+    QueueController<METRIC_CONTROL_PREDICTION_SRV, POTENTIAL_ACTION_SET_MSG, POTENTIAL_ACTION_SET_SRV, ACTION_LIB_OUTPUT_TYPE> queue_controller(nh);
 
     ros::spin();
 }
