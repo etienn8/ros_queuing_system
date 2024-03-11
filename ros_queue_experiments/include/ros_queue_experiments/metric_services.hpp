26de59f1427f745ea0025b1447b9120464c2c335
etienn8 <etienn8@gmail.com>
2024-03-11 16:59:46 -0400

    Added base class to hold the service server in the auv_system
    -Added the MetricServices class to serve has a base class for the metrics that needs to be fetched by the system.
    - The has a pointer towards  a AUVStateManager to have an interface to get the current state of the system via the getCurrentStates() method.
    - Four callbacks are defined. Two of them are used as the service callback and they each call a virtual method that will need to be overridden by a child class.
    - In the callback, two of them is linked to the real measure of the metric will the other one is the service to get a predicted value of the metric for given actions.

---
+++ ros_queue_experiments/include/ros_queue_experiments/metric_services.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "ros/ros.h"
+
+#include <string>
+#include <memory>
+
+#include "auv_state_manager.hpp"
+
+#include <ros_queue_msgs/FloatRequest.h>
+#include <ros_queue_msgs/MetricTransmissionVectorPredictions.h>
+#include <ros_queue_experiments/AuvStates.h>
+
+
+class MetricServices
+{
+    public:
+        MetricServices(ros::NodeHandle nh, std::string metric_name, std::shared_ptr<AUVStateManager> auv_state_manager);
+
+    protected:
+        std::string metric_name_;
+        
+        ros::NodeHandle nh_;
+
+        ros::ServiceServer real_metric_service_;
+        ros::ServiceServer expected_metric_service_;
+
+        std::shared_ptr<AUVStateManager> auv_state_manager_;
+
+        virtual bool realMetricCallback(ros_queue_msgs::FloatRequest::Request& req, 
+                                        ros_queue_msgs::FloatRequest::Response& res) = 0;
+        
+        virtual bool expectedMetricCallback(ros_queue_msgs::MetricTransmissionVectorPredictions::Request& req, 
+                                            ros_queue_msgs::MetricTransmissionVectorPredictions::Response& res) =0;
+
+        ros_queue_experiments::AuvStates getCurrentStates();
+    private:
+        bool realServiceMetricCallback(ros_queue_msgs::FloatRequest::Request& req, 
+                                       ros_queue_msgs::FloatRequest::Response& res);
+
+        bool expectedServiceMetricCallback(ros_queue_msgs::MetricTransmissionVectorPredictions::Request& req, 
+                                           ros_queue_msgs::MetricTransmissionVectorPredictions::Response& res);
+        
+        
+};
