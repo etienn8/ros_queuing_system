ed82b8fd337ea6f42e96e50d44d72d6e8ce8d6f0
Etienne <vile2094@usherbrooke.ca>
2024-06-05 17:07:05 -0400

    Added the extraction of the metric performances and the queue server end values

--- ros_queue_experiments/scripts/common_experiment_utils.py
+++ ros_queue_experiments/scripts/common_experiment_utils.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 import os
+import rosbag
+import rospy
+
+from ros_queue_experiments.msg import MetricPerformance
 
 script_path_list = os.path.normpath(os.path.abspath(__file__)).split(os.sep)
 package_path = script_path_list[:-2]
@@ -21,3 +25,63 @@ class QueueServerStatsStruct:
         self.low_temperature_stats = QueueServerMetricStatsStruct()
         self.real_queue_stats = QueueServerMetricStatsStruct()
 
+class MetricPerformanceStruct:
+    def __init__(self):
+        self.time_stamps = []
+
+        self.current_real_value = []
+        self.real_average_value = []
+        self.real_continous_average_value = []
+        self.real_time_average_value = []
+        self.queue_server_time_average_value = []
+        self.queue_server_arrival_mean = []
+        self.target_value = []
+        self.real_current_diff_with_target = []
+        self.real_continuous_average_diff_with_target = []
+        self.real_continuous_average_diff_with_server_mean = []
+        self.real_continuous_average_diff_with_server_time_average = []
+
+
+class AllMetricPerformanceStruct:
+    def __init__(self):
+        self.localization = MetricPerformanceStruct()
+        self.temperature = MetricPerformanceStruct()
+        self.real_queue = MetricPerformanceStruct()
+        self.penalty = MetricPerformanceStruct()
+
+    def populateWithBag(self, bag: rosbag.Bag, monitoring_prefix: str, time_init: rospy.Time):
+        self.__populateMetric(bag, monitoring_prefix + "monitoring_node/localization", self.localization, time_init)
+        self.__populateMetric(bag, monitoring_prefix + "monitoring_node/temperature", self.temperature, time_init)
+        self.__populateMetric(bag, monitoring_prefix + "monitoring_node/real_queue", self.real_queue, time_init)
+        self.__populateMetric(bag, monitoring_prefix + "monitoring_node/penalty", self.penalty, time_init)
+ 
+    def __populateMetric(self, bag: rosbag.Bag, topic_name: str, metric: MetricPerformanceStruct, time_init: float):
+        for topic, msg, t in bag.read_messages(topics=[topic_name]):
+            metric.time_stamps.append(t.to_sec() - time_init)
+            metric.current_real_value.append(msg.current_real_value)
+            metric.real_average_value.append(msg.real_average_value)
+            metric.real_continous_average_value.append(msg.real_continous_average_value)
+            metric.real_time_average_value.append(msg.real_time_average_value)
+            metric.queue_server_time_average_value.append(msg.queue_server_time_average_value)
+            metric.queue_server_arrival_mean.append(msg.queue_server_arrival_mean)
+            metric.target_value.append(msg.target_value)
+            metric.real_current_diff_with_target.append(msg.real_current_diff_with_target)
+            metric.real_continuous_average_diff_with_target.append(msg.real_continuous_average_diff_with_target)
+            metric.real_continuous_average_diff_with_server_mean.append(msg.real_continuous_average_diff_with_server_mean)
+            metric.real_continuous_average_diff_with_server_time_average.append(msg.real_continuous_average_diff_with_server_time_average)
+
+class QueueEndValues:
+    def __init__(self):
+        self.localization_arrival = 0.0
+        self.localization_departure = 0.0
+
+        self.temperature_arrival = 0.0
+        self.temperature_departure = 0.0
+
+        self.low_temperature_arrival = 0.0
+        self.low_temperature_departure = 0.0
+
+        self.real_queue_arrival = 0.0
+        self.real_queue_departure = 0.0
+
+        self.penalty = 0.0
--- ros_queue_experiments/scripts/experiment1_definition.py
+++ ros_queue_experiments/scripts/experiment1_definition.py
@@ -5,6 +5,7 @@ import rosbag
 from datetime import datetime
 import csv
 from ros_queue_msgs.msg import QueueServerStats
+from ros_queue_experiments.msg import MetricPerformance
 
 import common_experiment_utils
 
@@ -20,12 +21,17 @@ class Experiment1Analyser:
             "/NoRew_NoInv/auv_system_node/auv_state",
             "/NoRew_NoInv/monitoring_node/localization",
             "/NoRew_NoInv/monitoring_node/real_queue",
-            "/NoRew_NoInv/monitoring_node/temperature"]
+            "/NoRew_NoInv/monitoring_node/temperature",
+            "/NoRew_NoInv/monitoring_node/penalty"]
 
     def generateOutput(self, time_init):
         bag = rosbag.Bag(common_experiment_utils.BAG_DIRECTORY_PATH + self.bag_name + ".bag")
-        
+
+
+        # Get queue server end values
         queue_server_stats = common_experiment_utils.QueueServerStatsStruct()
+        queue_server_arrival_departures_end_values = common_experiment_utils.QueueEndValues()
+
         for topic, msg, t in bag.read_messages(topics=["/NoRew_NoInv/queue_server/server_stats"]):
             queue_server_stats.time_stamps.append((t.to_sec()-time_init))
             
@@ -46,8 +52,25 @@ class Experiment1Analyser:
                     queue_server_stats.real_queue_stats.queue_size.append(queue_stats.current_size)
                     queue_server_stats.real_queue_stats.time_average_arrival.append(queue_stats.arrival_time_average)
                     queue_server_stats.real_queue_stats.time_average_departure.append(queue_stats.departure_time_average)
+         
+        penalty_list = []
+        for topic, msg, t in bag.read_messages(topics=["/NoRew_NoInv/monitoring_node/penalty"]):
+            penalty_list.append(msg.queue_server_time_average_value)
+
+        queue_server_arrival_departures_end_values.localization_arrival = queue_server_stats.localization_stats.time_average_arrival[-1]
+        queue_server_arrival_departures_end_values.localization_departure = queue_server_stats.localization_stats.time_average_departure[-1]
+        queue_server_arrival_departures_end_values.temperature_arrival = queue_server_stats.temperture_stats.time_average_arrival[-1]
+        queue_server_arrival_departures_end_values.temperature_departure = queue_server_stats.temperture_stats.time_average_departure[-1]
+        queue_server_arrival_departures_end_values.low_temperature_arrival = queue_server_stats.low_temperature_stats.time_average_arrival[-1]
+        queue_server_arrival_departures_end_values.low_temperature_departure = queue_server_stats.low_temperature_stats.time_average_departure[-1]
+        queue_server_arrival_departures_end_values.real_queue_arrival = queue_server_stats.real_queue_stats.time_average_arrival[-1]
+        queue_server_arrival_departures_end_values.real_queue_departure = queue_server_stats.real_queue_stats.time_average_departure[-1]
+        queue_server_arrival_departures_end_values.penalty = penalty_list[-1]
         
-        
+        # Get performance metrics 
+        all_metric_performance_structs = common_experiment_utils.AllMetricPerformanceStruct()
+        all_metric_performance_structs.populateWithBag(bag, "/NoRew_NoInv/", time_init)
+
         rows = []
         for i in range(len(queue_server_stats.time_stamps)):
             row = []
@@ -60,11 +83,46 @@ class Experiment1Analyser:
             row.append(queue_server_stats.low_temperature_stats.time_average_departure[i])
             row.append(queue_server_stats.real_queue_stats.time_average_arrival[i])
             row.append(queue_server_stats.real_queue_stats.time_average_departure[i])
+
+            if (i == 0):
+                row.append(queue_server_arrival_departures_end_values.localization_arrival)
+                row.append(queue_server_arrival_departures_end_values.localization_departure)
+                row.append(queue_server_arrival_departures_end_values.temperature_arrival)
+                row.append(queue_server_arrival_departures_end_values.temperature_departure)
+                row.append(queue_server_arrival_departures_end_values.low_temperature_arrival)
+                row.append(queue_server_arrival_departures_end_values.low_temperature_departure)
+                row.append(queue_server_arrival_departures_end_values.real_queue_arrival)
+                row.append(queue_server_arrival_departures_end_values.real_queue_departure)
+                row.append(queue_server_arrival_departures_end_values.penalty)
+            else:
+                for j in range(9):
+                    row.append("")
+
             rows.append(row)
+        
         csv_filename = common_experiment_utils.RESULT_DIRECTORY_PATH + self.bag_name + ".csv"
+        
         with open(csv_filename, 'w') as csvfile:
             csvwriter = csv.writer(csvfile, delimiter=';')
-            csvwriter.writerow(["time", "localization_arrival", "localization_departure", "temperature_arrival", "temperature_departure", "low_temperature_arrival", "low_temperature_departure", "real_queue_arrival", "real_queue_departure"])
+            csvwriter.writerow(["time", 
+                               "localization_arrival", 
+                               "localization_departure", 
+                               "temperature_arrival", 
+                               "temperature_departure", 
+                               "low_temperature_arrival", 
+                                "low_temperature_departure",
+                                "real_queue_arrival", 
+                                "real_queue_departure",
+                                "last_localization_arrival",
+                                "last_localization_departure",
+                                "last_temperature_arrival",
+                                "last_temperature_departure",
+                                "last_low_temperature_arrival",
+                                "last_low_temperature_departure",
+                                "last_real_queue_arrival",
+                                "last_real_queue_departure",
+                                "penalty"])
+            
             csvwriter.writerows(rows)
 
     def prefixToBagName(self, prefix):
