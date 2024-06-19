659971a5c5cdb27434ff412110dd6232482a3bb5
Etienne <vile2094@usherbrooke.ca>
2024-06-19 15:37:09 -0400

    Added the graphics of the estimation errors of the low-temperature queue and the real queue to the experiment 2

--- ros_queue_experiments/scripts/experiment2_definition.py
+++ ros_queue_experiments/scripts/experiment2_definition.py
@@ -61,6 +61,14 @@ class Experiment2Analyser:
         # Get performance metrics 
         perturbation_all_metric_performance_structs = common_experiment_utils.AllMetricPerformanceStruct()
         perturbation_all_metric_performance_structs.populateWithBag(bag, "/NoRew_NoInv/", perturbation_time_init)
+        
+        normal_task_queue_departure_arrival_mean_diff = common_experiment_utils.Series()
+        normal_task_queue_departure_arrival_mean_diff.variable_name = "Departure and arrival diff NoRew_NoInv"
+        normal_task_queue_departure_arrival_mean_diff.values = [departure - arrival for arrival, departure in zip(normal_all_metric_performance_structs.real_queue.queue_server_arrival_mean.values, normal_all_metric_performance_structs.real_queue.target_value.values)]
+        
+        perturbation_task_queue_departure_arrival_mean_diff = common_experiment_utils.Series()
+        perturbation_task_queue_departure_arrival_mean_diff.variable_name = "Departure and arrival diff NoRew_NoInv with perturbation"
+        perturbation_task_queue_departure_arrival_mean_diff.values = [departure - arrival for arrival, departure in zip(perturbation_all_metric_performance_structs.real_queue.queue_server_arrival_mean.values, perturbation_all_metric_performance_structs.real_queue.target_value.values)]
 
         # ====== Create output CSV ======
         normal_data_separator = common_experiment_utils.Series()
@@ -80,6 +88,7 @@ class Experiment2Analyser:
                             normal_all_metric_performance_structs.real_queue.time_stamps,
                             normal_all_metric_performance_structs.real_queue.queue_server_arrival_mean,
                             normal_all_metric_performance_structs.real_queue.target_value,
+                            normal_task_queue_departure_arrival_mean_diff,
                             normal_action_performances.time_stamps,
                             common_experiment_utils.actionSeriesToStringSeries(normal_action_performances.applied_action),
                             common_experiment_utils.actionSeriesToStringSeries(normal_action_performances.selected_action),
@@ -95,6 +104,7 @@ class Experiment2Analyser:
                             perturbation_all_metric_performance_structs.real_queue.time_stamps,
                             perturbation_all_metric_performance_structs.real_queue.queue_server_arrival_mean,
                             perturbation_all_metric_performance_structs.real_queue.target_value,
+                            perturbation_task_queue_departure_arrival_mean_diff,
                             perturbation_action_performances.time_stamps,
                             common_experiment_utils.actionSeriesToStringSeries(perturbation_action_performances.applied_action),
                             common_experiment_utils.actionSeriesToStringSeries(perturbation_action_performances.selected_action),
@@ -154,6 +164,47 @@ class Experiment2Analyser:
         ax4.grid(False)
         ax4.legend()
         ax4.set_xlabel("Time (s)")
+
+        fig3, (ax5, ax6) = plt.subplots(2, 1, sharex=True)
+        fig3.suptitle("Estimation error of the queue server with the real metric mean for the low temperature metric of a min drift plus penalty with and without action uncertainties")
+
+        ax5.set_title("Absolute error between the real metric mean and the estimated metric mean from the queue server metric over time")
+        ax5.plot(normal_all_metric_performance_structs.low_temperature.time_stamps.values,
+                    normal_all_metric_performance_structs.low_temperature.real_continuous_average_diff_with_server_mean.values,
+                    label="Without perturbation")
+        ax5.plot(perturbation_all_metric_performance_structs.low_temperature.time_stamps.values,
+                    perturbation_all_metric_performance_structs.low_temperature.real_continuous_average_diff_with_server_mean.values,
+                    label="With perturbations")
+        ax5.set_ylabel("Estimation error (C)")
+        ax5.grid(False)
+        ax5.legend()
+
+        ax6.set_title("Absolute error between the real metric mean and the mean target for the low temperature")
+        ax6.plot(normal_all_metric_performance_structs.low_temperature.time_stamps.values,
+                    normal_all_metric_performance_structs.low_temperature.real_continuous_average_diff_with_target.values,
+                    label="Without perturbation")
+        ax6.plot(perturbation_all_metric_performance_structs.low_temperature.time_stamps.values,
+                    perturbation_all_metric_performance_structs.low_temperature.real_continuous_average_diff_with_target.values,
+                    label="With perturbations")
+        ax6.set_ylabel("Error (C)")
+        ax6.grid(False)
+        ax6.legend()
+        ax6.set_xlabel("Time (s)")
+
+        fig4, ax7 = plt.subplots(1, 1, sharex=True)
+        fig4.suptitle("Task queue departure mean difference with its arrivals mean for a min drift plus penalty with and without action uncertainties")
+        ax7.set_title("Difference between the queue server arrival mean and the target value over time")
+        
+        ax7.plot(normal_all_metric_performance_structs.real_queue.time_stamps.values,
+                    normal_task_queue_departure_arrival_mean_diff.values,
+                    label="Without perturbation")
+        ax7.plot(perturbation_all_metric_performance_structs.real_queue.time_stamps.values,
+                    perturbation_task_queue_departure_arrival_mean_diff.values,
+                    label="With perturbations")
+        ax7.set_ylabel("Difference (Task/s)")
+        ax7.grid(False)
+        ax7.legend()
+        ax7.set_xlabel("Time (s)")
         plt.show()
 
     def getNormalBagName(self):
