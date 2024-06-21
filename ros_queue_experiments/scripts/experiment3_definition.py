a66e64034c352f16600ecf0ff090f49ed0f2064c
Etienne <vile2094@usherbrooke.ca>
2024-06-21 14:07:05 -0400

    Added a third experiment to produce a graph of estimation errors of all controller for one metric
    - Added a new experiment analyzer that listen to all metrics for a controller in a given situation.
    - The analyzer output generation stores all the metric performance for each controller from the given rosbag. It also stores the end values of all the metrics inside a memebr of the class to be retrieved later by another analyzer.
    - Added the experiment 3 in the launcher and start it with the bad_prediction_setup to see the influence of different controller given a model perturbation.

---
+++ ros_queue_experiments/scripts/experiment3_definition.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+import rospy
+
+import rosbag
+from datetime import datetime
+import csv
+import matplotlib.pyplot as plt
+
+import common_experiment_utils
+
+class Experiment3Analyser:
+    def __init__(self):
+        time_now = datetime.now()
+        string_time_now = time_now.strftime("%Y-%m-%d_%H-%M-%S")
+        self.bag_name = "experiment3_" + string_time_now
+
+        self.sub_topics_to_record = [
+            "monitoring_node/localization",
+            "monitoring_node/real_queue",
+            "monitoring_node/temperature",
+            "monitoring_node/low_temperature",
+            "monitoring_node/penalty"]
+
+        self.topics_to_record = []
+        for controller_type in common_experiment_utils.controller_type_list:
+            for sub_topic in self.sub_topics_to_record:
+                self.topics_to_record.append("/" + controller_type + "/" + sub_topic)
+
+    def generateOutput(self, time_init, bag_name, base_init_time_on_first_value=False):
+        bag = rosbag.Bag(common_experiment_utils.BAG_DIRECTORY_PATH + bag_name + ".bag")
+
+        # Get init time if not set
+        init_time_set = False
+
+        for topic, msg, t in bag.read_messages(topics=["/NoRew_NoInv/queue_server/server_stats"]):
+            if (not init_time_set) and base_init_time_on_first_value:
+                time_init = t.to_sec()
+                init_time_set = True
+                break
+
+        # Get performance metrics 
+        controller_performance_metrics = {"NoRew_NoInv": common_experiment_utils.AllMetricPerformanceStruct(),
+                                          "NoRew_Inv": common_experiment_utils.AllMetricPerformanceStruct(),
+                                          "Rew_NoInv": common_experiment_utils.AllMetricPerformanceStruct(),
+                                          "Rew_Inv": common_experiment_utils.AllMetricPerformanceStruct()}
+        
+        for controller_type in common_experiment_utils.controller_type_list:
+            controller_performance_metrics[controller_type].populateWithBag(bag, "/" + controller_type + "/", time_init)
+
+        # Get end values of the metrics
+        self.multi_controller_end_struct = common_experiment_utils.MultiControllerEndMetricStruct()
+        for controller_type in common_experiment_utils.controller_type_list:
+            controller_performance_metric_dict = {"localization": controller_performance_metrics[controller_type].localization,
+                                                  "temperature": controller_performance_metrics[controller_type].temperature,
+                                                  "low_temperature": controller_performance_metrics[controller_type].low_temperature,
+                                                  "real_queue": controller_performance_metrics[controller_type].real_queue,
+                                                  "penalty": controller_performance_metrics[controller_type].penalty}
+             
+            controller_end_struct = self.multi_controller_end_struct.controller_end_metrics[controller_type]
+
+            for metric_name in common_experiment_utils.metric_type_list:
+                controller_end_struct.metric[metric_name].estimation_error.values = [controller_performance_metric_dict[metric_name].absolute_real_continuous_average_diff_with_server_mean.values[-1]]
+                controller_end_struct.metric[metric_name].target_error.values = [controller_performance_metric_dict[metric_name].real_continuous_average_diff_with_target.values[-1]]
+
+        # Create output CSV
+        separator_second_graph = common_experiment_utils.Series()
+        separator_second_graph.variable_name = "controller_sacrifices_one_queue"
+        separator_end_values_table = common_experiment_utils.Series()
+        separator_end_values_table.variable_name = "end_values_table"
+
+        csv_filename = common_experiment_utils.RESULT_DIRECTORY_PATH + self.bag_name + ".csv"
+        series_to_record = []
+        for controller_type in common_experiment_utils.controller_type_list:
+            controller_separator = common_experiment_utils.Series()
+            controller_separator.variable_name = controller_type
+            series_to_record += [controller_separator,
+                                 controller_performance_metrics[controller_type].localization.time_stamps,
+                                 controller_performance_metrics[controller_type].localization.real_continous_average_value,
+                                 controller_performance_metrics[controller_type].localization.target_value,
+                                 controller_performance_metrics[controller_type].localization.absolute_real_continuous_average_diff_with_server_mean,
+                                 controller_performance_metrics[controller_type].localization.real_continuous_average_diff_with_target,
+                                 controller_performance_metrics[controller_type].temperature.time_stamps,
+                                 controller_performance_metrics[controller_type].temperature.real_continous_average_value,
+                                 controller_performance_metrics[controller_type].temperature.target_value,
+                                 controller_performance_metrics[controller_type].temperature.absolute_real_continuous_average_diff_with_server_mean,
+                                 controller_performance_metrics[controller_type].temperature.real_continuous_average_diff_with_target,
+                                 controller_performance_metrics[controller_type].low_temperature.target_value,
+                                 controller_performance_metrics[controller_type].low_temperature.real_continuous_average_diff_with_target,
+                                 controller_performance_metrics[controller_type].real_queue.time_stamps,
+                                 controller_performance_metrics[controller_type].real_queue.queue_server_arrival_mean,
+                                 controller_performance_metrics[controller_type].real_queue.target_value,
+                                 controller_performance_metrics[controller_type].real_queue.absolute_real_continuous_average_diff_with_server_mean,
+                                 controller_performance_metrics[controller_type].penalty.time_stamps,
+                                 controller_performance_metrics[controller_type].penalty.real_time_average_value]
+        
+        common_experiment_utils.createCSV(series_to_record, csv_filename)
+
+        # ==== Create plots ====
+        fig1, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+        ax1.set_title("Estimation error between the real metric mean and the queue server mean for the localization metric for all controllers over time with model uncertainties")
+        for controller_type in common_experiment_utils.controller_type_list:
+            metric_performance_struct = controller_performance_metrics[controller_type].localization
+            ax1.plot(metric_performance_struct.time_stamps.values, 
+                     metric_performance_struct.absolute_real_continuous_average_diff_with_server_mean.values, 
+                     label=controller_type)
+    
+        ax1.set_ylabel("Localization estimation error (m)")
+        ax1.grid(True)
+        ax1.legend()
+
+        ax2.set_title("Error between the real metric mean and the target value for the localization metric for all controllers over time with model uncertainties")
+        for controller_type in common_experiment_utils.controller_type_list:
+            metric_performance_struct = controller_performance_metrics[controller_type].localization
+            ax2.plot(metric_performance_struct.time_stamps.values, 
+                     metric_performance_struct.real_continuous_average_diff_with_target.values, 
+                     label=controller_type)
+        
+        ax2.set_ylabel("Localization error (m)")
+        ax2.grid(True)
+        ax2.legend()
+        ax2.set_xlabel("Time (s)")
+        
+
+    def prefixToBagName(self, prefix):
+        self.bag_name = prefix + self.bag_name
+
+    def getBagName(self):
+        return self.bag_name
+    
+    def getROSBagArguments(self):
+        topic_args = ""
+        for topic in self.topics_to_record:
+            topic_args += " " + topic
+        
+        return "-O "+ common_experiment_utils.BAG_DIRECTORY_PATH + self.bag_name + ".bag" + topic_args
+
--- ros_queue_experiments/scripts/experiment_manager.py
+++ ros_queue_experiments/scripts/experiment_manager.py
@@ -12,6 +12,7 @@ import common_experiment_utils
 import experiment_instance
 import experiment1_definition
 import experiment2_definition
+import experiment3_definition
 
 import matplotlib.pyplot as plt
 import scienceplots
@@ -29,21 +30,31 @@ if __name__ == "__main__":
 
     # Experiment 1
 
-    # cli_args = ['experiment_setup:=perfect_model_and_setup']
+    cli_args = ['experiment_setup:=perfect_model_and_setup']
 
-    # exp1_analyser = experiment1_definition.Experiment1Analyser()
-    # exp1 = experiment_instance.ExperimentInstance(uuid, cli_args, 60, exp1_analyser)
-    # exp1.execute(generate_output=False)
-    # #exp1.analyser.generateOutput(0, "experiment1_2024-06-10_14-55-02", base_init_time_on_first_value=True)
-    # exp1.analyser.generateOutput(exp1.time_init.to_sec(), exp1.analyser.getBagName())
-    # rospy.loginfo("Experiment 1 completed")
+    exp1_analyser = experiment1_definition.Experiment1Analyser()
+    exp1 = experiment_instance.ExperimentInstance(uuid, cli_args, 60, exp1_analyser)
+    #exp1.execute(generate_output=True)
+    exp1.analyser.generateOutput(0, "experiment1_2024-06-21_13-03-07", base_init_time_on_first_value=True)
+    rospy.loginfo("Experiment 1 completed")
 
     # Experiment 2
     exp2_analyser = experiment2_definition.Experiment2Analyser()
     exp2 = experiment_instance.Experiment2Instance(uuid, 90, exp2_analyser)
-    #exp2.execute(generate_output=False)
-    exp2.analyser.generateOutput(0,0, "normal_experiment2_2024-06-19_15-03-02", "perturbation_experiment2_2024-06-19_15-03-02",base_init_time_on_first_value=True)
+    #exp2.execute(generate_output=True)
+    exp2.analyser.generateOutput(0,0, "normal_experiment2_2024-06-21_13-04-13", "perturbation_experiment2_2024-06-21_13-04-13",base_init_time_on_first_value=True)
+    rospy.loginfo("Experiment 2 completed")
+
+    # Experiment 3
+    # Graph experiment
+    exp3_analyser = experiment3_definition.Experiment3Analyser()
+    exp3 = experiment_instance.ExperimentInstance(uuid, ['experiment_setup:=bad_prediction_model'], 90, exp3_analyser)
+    #exp3.execute(generate_output=True)
+    exp3.analyser.generateOutput(0, "experiment3_2024-06-21_13-07-26", base_init_time_on_first_value=True)
+    rospy.loginfo("Experiment 3 completed")
+
     plt.show()
+    
      
     
 
